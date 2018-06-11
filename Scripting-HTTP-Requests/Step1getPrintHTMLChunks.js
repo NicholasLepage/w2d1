f0f3951421f141c29391b56ff3940ee09d142666
Nicholas Lepage <nic.lep@hotmail.com>
2018-06-11 15:28:57 +0000

    Did Step1 of the Scripting HTTP Requests assignment

---
+++ Scripting-HTTP-Requests/Step1getPrintHTMLChunks.js
@@ -0,0 +1,22 @@
+function getAndPrintHTMLChunks () {
+  var https = require("https");
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response) {
+
+    response.setEncoding("utf8");
+
+    response.on("data", function(data) {
+      console.log("Here is the chunk information: \n", data);
+    });
+
+    response.on("end", function() {
+      console.log("We done here");
+    });
+  });
+}
+
+console.log(getAndPrintHTMLChunks());
