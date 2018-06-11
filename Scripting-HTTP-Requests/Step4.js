168db9a1730c3ef349705b9ba3abfa5a424f5cc8
Nicholas Lepage <nic.lep@hotmail.com>
2018-06-11 16:37:45 +0000

    Did Step4!

---
+++ Scripting-HTTP-Requests/Step4.js
@@ -0,0 +1,29 @@
+function getHTML (options, callback) {
+
+  var https = require("https");
+  var chunks = "";
+  https.get(options, function(response) {
+
+    response.setEncoding("utf8");
+
+    response.on("data", function(data) {
+      chunks += data;
+    });
+
+    response.on("end", function() {
+      callback(chunks);
+      console.log("We done here");
+    });
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
